beb8bd2ec6a95dcd6d2635e4cd2c07a711fa7def
Sebastian <basti.kunst@gmail.com>
2019-04-15 10:39:01 +0200

    TLC for 4.1

--- SortingAlgosVar/Program.cs
+++ SortingAlgosVar/Program.cs
@@ -18,91 +18,91 @@ namespace SortingAlgosVar
                 {
                     arr[i] = ran.Next(100);
                 }
-                //    Console.WriteLine("please decide on sorting algo. \nb for bubble, \ni for insertion, \nq for Quicksort, \nor the large letter for the adapted algo \npress x to exit");
+                Console.WriteLine("please decide on sorting algo. \nb for bubble, \ni for insertion, \nq for Quicksort,\ns for SelectionSort,\nor the large letter for the adapted algo \npress x to exit");
 
-                //    inputc = Console.ReadKey().KeyChar;
-                //    switch (inputc)
-                //    {
-                //        case 'i':
-                //            input = SortingAlgos.algos.i;
-                //            break;
-                //        case 'I':
-                //            input = SortingAlgos.algos.I;
-                //            break;
-                //        case 'b':
-                //            input = SortingAlgos.algos.b;
-                //            break;
-                //        case 'B':
-                //            input = SortingAlgos.algos.B;
-                //            break;
-                //        case 's':
-                //            input = SortingAlgos.algos.s;
-                //            break;
-                //        case 'S':
-                //            input = SortingAlgos.algos.S;
-                //            break;
-                //        case 'q':
-                //            input = SortingAlgos.algos.q;
-                //            break;
-                //        case 'Q':
-                //            input = SortingAlgos.algos.Q;
-                //            break;
-                //        case 'x':
-                //            cont = false;
-                //            break;
-                //        default:
-                //            break;
-                //    }
-                //    Console.WriteLine();
-                //    Algorithm.printArray(arr, arr.Length);
-                //    switch (input)
-                //    {
-                //        case SortingAlgos.algos.b:
-                //            Algorithm.BubbleSort(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.i:
-                //            Algorithm.InsertionSort(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.s:
-                //            Algorithm.SelectionSort(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.q:
-                //            Algorithm.QuickSort(arr, 0, arr.Length - 1);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.B:
-                //            Algorithm.BubbleSortRev(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.I:
-                //            Algorithm.InsertionSortRev(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.S:
-                //            Algorithm.SelectionSortRev(arr);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.Q:
-                //            Algorithm.QuickSortRand(arr, 0, arr.Length - 1);
-                //            Algorithm.printArray(arr, arr.Length);
-                //            break;
-                //        case SortingAlgos.algos.d:
-                //        default:
-                //            break;
-                //    }
-                //    Console.ReadKey();
-
-
+                inputc = Console.ReadKey().KeyChar;
+                switch (inputc)
+                {
+                    case 'i':
+                        input = SortingAlgos.algos.i;
+                        break;
+                    case 'I':
+                        input = SortingAlgos.algos.I;
+                        break;
+                    case 'b':
+                        input = SortingAlgos.algos.b;
+                        break;
+                    case 'B':
+                        input = SortingAlgos.algos.B;
+                        break;
+                    case 's':
+                        input = SortingAlgos.algos.s;
+                        break;
+                    case 'S':
+                        input = SortingAlgos.algos.S;
+                        break;
+                    case 'q':
+                        input = SortingAlgos.algos.q;
+                        break;
+                    case 'Q':
+                        input = SortingAlgos.algos.Q;
+                        break;
+                    case 'x':
+                        cont = false;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine();
                 Algorithm.printArray(arr, arr.Length);
-                Console.WriteLine("please enter an integer to be checked if it can be built as a sum by the members of the array");
-                int sum = int.Parse(Console.ReadLine());
-                if (Algorithm.SumCheck(arr, sum))
-                    Console.WriteLine("yep");
-                else
-                    Console.WriteLine("nope");
+                switch (input)
+                {
+                    case SortingAlgos.algos.b:
+                        Algorithm.BubbleSort(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.i:
+                        Algorithm.InsertionSort(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.s:
+                        Algorithm.SelectionSort(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.q:
+                        Algorithm.QuickSort(arr, 0, arr.Length - 1);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.B:
+                        Algorithm.BubbleSortRev(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.I:
+                        Algorithm.InsertionSortMin(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.S:
+                        Algorithm.SelectionSortRev(arr);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.Q:
+                        Algorithm.QuickSortRand(arr, 0, arr.Length - 1);
+                        Algorithm.printArray(arr, arr.Length);
+                        break;
+                    case SortingAlgos.algos.d:
+                    default:
+                        break;
+                }
+                Console.ReadKey();
+
+
+                //Algorithm.printArray(arr, arr.Length);
+                //Console.WriteLine("please enter an integer to be checked if it can be built as a sum by the members of the array");
+                //int sum = int.Parse(Console.ReadLine());
+                //if (Algorithm.SumCheck(arr, sum))
+                //    Console.WriteLine("yep");
+                //else
+                //    Console.WriteLine("nope");
             }
         }
     }
@@ -196,81 +196,92 @@ namespace SortingAlgosVar
         {
             int n = arr.Length;
             int temp;
-            for (int i = n; i >= 0; i--)
+            for (int i = n-1; i >= 0; i--)
             {
-                for (int j = n; j > i + 1; j--)
+                for (int j = 0; j < i ; j++)
                 {
-                    if (arr[j] > arr[j - 1])
+                    if (arr[j] > arr[j +1])
                     {
                         temp = arr[j];
-                        arr[j] = arr[j - 1];
-                        arr[j - 1] = temp;
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
         }
-        public void InsertionSortRev(int[] arr)
+        public void InsertionSortMin(int[] arr)
         {
             int n = arr.Length;
-            for (int i = 1; i < n; i++)
+            for (int i = n-2; i >= 0; i--)
             {
                 int key = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j] < key)
+                int j = i +1;
+                while (j < n && arr[j] < key)
                 {
-                    arr[j + 1] = arr[j];
-                    j -= 1;
+                    arr[j - 1] = arr[j];
+                    j++;
                 }
-                arr[j + 1] = key;
+                arr[j - 1] = key;
             }
         }
         public void SelectionSortRev(int[] arr)
         {
             int n = arr.Length;
-            int temp;
-            for (int i = 0; i < n; i++)
+            int temp, max;
+            for (int i = n-1; i >= 0; i--)
             {
-                int max = i;
-                for (int j = i + 1; j < n; j++)
+                max = i;
+                for (int j = i; j >=0; j--)
                 {
                     if (arr[j] > arr[max])
                     {
                         max = j;
                     }
-                    temp = arr[max];
-                    arr[max] = arr[i];
-                    arr[i] = temp;
+                    temp = arr[i];
+                    arr[i] = arr[max];
+                    arr[max] = temp;
                 }
             }
         }
-        public void QuickSortRand(int[] arr, int low, int high)
+        public void QuickSortRand(int[] arr, int left, int right)
         {
-            if (low < high)
+            if (left < right)
             {
-                int PartIn = QuickSortRandPart(arr, low, high);
-                QuickSortRand(arr, low, PartIn - 1);
-                QuickSortRand(arr, PartIn + 1, high);
+                int PartIn = QuickSortRandPart(arr, left, right);
+                QuickSortRand(arr, left, PartIn - 1);
+                QuickSortRand(arr, PartIn + 1, right);
             }
         }
-        private int QuickSortRandPart(int[] arr, int low, int high)
+        private int QuickSortRandPart(int[] arr, int left, int right)
         {
             Random rand = new Random();
-            int pivot = arr[rand.Next(low + 1, high)];
-            int i = (low - 1);
-            for (int j = low; j < high; j++)
+            int i = rand.Next(left, right);
+            int pivot = arr[i];
+            arr[i] = arr[right];
+            arr[right] = pivot;
+
+            return QuickSortRandPartition(arr, left, right);
+        }
+
+        private int QuickSortRandPartition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int temp;
+            int i = left;
+
+            for (int j = left; j < right; j++)
             {
-                if (arr[j] <= pivot)
+                if (arr[j]<=pivot)
                 {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                     i++;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
                 }
             }
-            int temp1 = arr[i + 1];
-            arr[i + 1] = arr[high];
-            arr[high] = temp1;
-            return i + 1;
+            arr[right] = arr[i];
+            arr[i] = pivot;
+            return i;
         }
 
         public void printArray(int[] arr, int n)
